3c216ccf9cf01fc32398a959613e3e85173e1069
Muhlis <muhlisbc@gmail.com>
2017-03-29 13:14:24 +0700

    Add @link_text in html email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,7 @@ class UserMailer < ApplicationMailer
     @title  = "Please confirm your account"
     @text   = "Please click the link below to confirm your account:"
     @link   = confirm_email_account_url(token: Crypt.encrypt(email))
+    @link_text = "Confirm your account"
     mail(to: @to, subject: [@title, @to].join(", "))
   end
 
@@ -13,6 +14,7 @@ class UserMailer < ApplicationMailer
     @title  = "Reset password request"
     @text   = "Please click the link below to reset your password:"
     @link   = reset_password_account_url(token: Crypt.encrypt(email), digest: Crypt.encrypt(password_digest))
+    @link_text = "Reset your password"
     mail(to: @to, subject: [@title, @to].join(", "))
   end
 
